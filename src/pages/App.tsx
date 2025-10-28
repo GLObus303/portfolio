adf8d143365fea9df47d29f6db07329e2ef11c34
Lukas Cizek <globus303@gmail.com>
2025-10-28 21:28:12 +0100

    Add basic layout (#3)

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,24 @@
+import react from '../assets/react.svg';
 import style from './App.module.scss';
 
-const App = () => <h1 className={style.title}>Portfolio</h1>;
+const App = () => (
+  <>
+    <header className={style.header}>
+      Lukas Cizek
+      <div className={style.logo}>
+        <p>
+          10<sup>-10</sup>m
+        </p>
+        <img className={style.logoIcon} src={react} alt="React" />
+      </div>
+    </header>
+    <main className={style.content}>
+      <hgroup className={style.titleGroup}>
+        <h1 className={style.name}>Lukas Cizek</h1>
+        <p className={style.title}>Senior Fullstack Developer | Team Leader</p>
+      </hgroup>
+    </main>
+  </>
+);
 
 export default App;
