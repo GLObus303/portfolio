c2e90e29fee15ffe9468ebeed34c2cbc2cb8a7c9
Lukas Cizek <globus303@gmail.com>
2025-11-10 20:02:00 +0100

    Add hero section

---
+++ src/components/Experience.tsx
@@ -0,0 +1,7 @@
+import style from './Experience.module.scss';
+
+export const Experience: React.FC = () => (
+  <section className={style.experience}>
+    <h2 className={style.title}>Experience</h2>
+  </section>
+);
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,24 +1,10 @@
-import react from '../assets/react.svg';
+import { Hero } from '../components/Hero';
 import style from './App.module.scss';
 
 const App = () => (
-  <>
-    <header className={style.header}>
-      Lukas Cizek
-      <div className={style.logo}>
-        <p>
-          10<sup>-10</sup>m
-        </p>
-        <img className={style.logoIcon} src={react} alt="React" />
-      </div>
-    </header>
-    <main className={style.content}>
-      <hgroup className={style.titleGroup}>
-        <h1 className={style.name}>Lukas Cizek</h1>
-        <p className={style.title}>Senior Fullstack Developer | Team Leader</p>
-      </hgroup>
-    </main>
-  </>
+  <main className={style.content}>
+    <Hero />
+  </main>
 );
 
 export default App;
